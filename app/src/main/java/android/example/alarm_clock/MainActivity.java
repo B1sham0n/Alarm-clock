6aa0abfc6f730b122fbbc076f71e88674769244f
Valeriya <valeriya.ridchenko@mail.ru>
2019-06-17 17:32:36 +0300

    new buttons and delete alarms

--- app/src/main/java/android/example/alarm_clock/MainActivity.java
+++ app/src/main/java/android/example/alarm_clock/MainActivity.java
@@ -3,14 +3,20 @@ package android.example.alarm_clock;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Switch;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
     Button btnPlusHour, btnMinusHour, btnPlusMinute, btnMinusMinute, btnAdd;
@@ -20,6 +26,8 @@ public class MainActivity extends AppCompatActivity {
     LinearLayout mainChild;
     TextView addTVHour, addTVMinute;
     Button addBtnDel, addBtnOn;
+    ArrayList<View> viewList;
+    Switch addSwitch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -68,29 +76,38 @@ public class MainActivity extends AppCompatActivity {
                    hour = 0;
                else
                     hour++;
-               tvHour.setText(Integer.toString(hour));
+
            }
            if(view == btnMinusHour){
                if(hour > 0)
                    hour--;
                else
                    hour = 23;
-                tvHour.setText(Integer.toString(hour));
            }
            if(view == btnPlusMinute){
                if(minute == 59)
                    minute = 0;
                else
                    minute++;
-                tvMinute.setText(Integer.toString(minute));
            }
            if(view == btnMinusMinute){
                if(minute > 0)
                    minute--;
                else
                    minute = 59;
-                tvMinute.setText(Integer.toString(minute));
            }
+            if(hour < 10) {
+                tvHour.setText("0");
+                tvHour.append(Integer.toString(hour));
+            }
+            else
+                tvHour.setText(Integer.toString(hour));
+            if(minute < 10) {
+                tvMinute.setText("0");
+                tvMinute.append(Integer.toString(minute));
+            }
+            else
+                tvMinute.setText(Integer.toString(minute));
         }
     };
     View.OnClickListener btnAddTime = new View.OnClickListener() {
@@ -103,13 +120,55 @@ public class MainActivity extends AppCompatActivity {
             LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             childView = inflater.inflate(R.layout.child, null);
             addTVHour =  childView.findViewById(R.id.newHour);
-            addTVHour.setText(Integer.toString(hour));
+            if(hour < 10)
+            {
+                addTVHour.setText("0");
+                addTVHour.append(Integer.toString(hour));
+            }
+            else
+                addTVHour.setText(Integer.toString(hour));
+
             addTVMinute =  childView.findViewById(R.id.newMinute);
-            addTVMinute.setText(Integer.toString(minute));
+            if(minute < 10)
+            {
+                addTVMinute.setText("0");
+                addTVMinute.append(Integer.toString(minute));
+            }
+            else
+                addTVMinute.setText(Integer.toString(minute));
+
+            addBtnDel = childView.findViewById(R.id.newBtnDel);
+            //addBtnOn = childView.findViewById(R.id.newBtnOn);
+
+            addBtnDel.setOnClickListener(btnRemove);
+            addBtnDel.setTag(root.getChildCount());
+
+            addSwitch = childView.findViewById(R.id.newSwitch);
+            addSwitch.setOnCheckedChangeListener(switchOn);
+
 
             root.addView(childView);
+            //viewList.add(childView);
+
+            addTVMinute = root.getChildAt(1).findViewById(R.id.newMinute);
             int count = root.getChildCount();//getChildAt()
-            System.out.println(count);
+            System.out.println("___" + addTVMinute.getText());
+            //if (count > 3)
+            //    root.removeViewAt(2);
+        }
+    };
+    View.OnClickListener btnRemove = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            root.removeViewAt((Integer) view.getTag());
+            System.out.println(view.getTag());
+        }
+    };
+    OnCheckedChangeListener switchOn = new OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+            if(addSwitch.isChecked())
+                System.out.println("_123123");
         }
     };
 }
